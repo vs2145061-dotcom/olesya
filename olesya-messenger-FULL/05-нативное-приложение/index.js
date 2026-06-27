fa890b042209d95b72a1d1e6f40ac9c586736a8d
vs2145061-dotcom <vs2145061@gmail.com>
2026-06-27 04:14:28 -0700

    Delete olesya-messenger-FULL/05-нативное-приложение directory

--- olesya-messenger-FULL/05-нативное-приложение/index.js
+++
@@ -1,5 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// Точка входа Expo. package.json: "main": "index.js"
-registerRootComponent(App);
--- olesya-messenger-FULL/05-нативное-приложение/metro.config.js
+++
@@ -1,9 +0,0 @@
-// Metro: разрешить require() HTML-файла (локальный резерв в App.js: require('./assets/www/index.html'))
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-if (!config.resolver.assetExts.includes('html')) {
-  config.resolver.assetExts.push('html');
-}
-
-module.exports = config;
